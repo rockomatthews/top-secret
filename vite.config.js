e021c2b6b2aec0a871b44fa43ca9757f197a6029
rockomatthews <rob@robmatthe.ws>
2024-08-15 19:16:38 -0500

    deploy to vercel

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build',
+    outDir: 'dist', // This should match Vercel's expected output directory
   },
   resolve: {
     extensions: ['.js', '.jsx', '.json'],
